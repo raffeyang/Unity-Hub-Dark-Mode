bf9e8c67fe0442c543637770cfd3cf6357cd54a4
JY <ithinkinx7@gmail.com>
2020-05-15 08:09:41 +0800

    Update

--- app/src/services/localConfig/localConfig.js
+++
@@ -1,8 +0,0 @@
-const defaultConfig = {
-  [settings.keys.DISABLE_CLOUD_PROJECTS]: true,
-  [settings.keys.DISABLE_ANALYTICS]: false,
-  [settings.keys.DISABLE_SIGNIN]: false,
-  [settings.keys.DISABLE_LEARN]:true,
-  [settings.keys.DISABLE_AUTO_UPDATE]: false,
-  [settings.keys.DISABLE_ELEVATE]: false,
-};
